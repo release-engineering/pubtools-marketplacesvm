4571f911c6b6a6d56bd3d27d23ed45c77bdad992
Jonathan Gangi <jgangi@redhat.com>
2023-01-27 14:23:26 -0300

    Add tests for CloudProviders
    This commit include the initial tests for CloudProviders.

---
+++ tests/cloud_providers/test_provider_interface.py
@@ -0,0 +1,56 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+import logging
+from unittest.mock import create_autospec
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+
+from pubtools._marketplacesvm.cloud_providers import CloudCredentials, CloudProvider, get_provider
+from pubtools._marketplacesvm.cloud_providers.base import VMIPushItem
+
+from .conftest import FakeProvider
+
+
+def test_get_provider_invalid(caplog: LogCaptureFixture) -> None:
+    expected_err = "Missing or invalid credentials."
+    with caplog.at_level(logging.ERROR):
+        with pytest.raises(RuntimeError, match=expected_err):
+            get_provider({"marketplace_account": "foo", "auth": {}})
+
+
+def test_get_provider_notfound(caplog: LogCaptureFixture) -> None:
+    expected_err = "No provider found for cloud-emea"
+    with caplog.at_level(logging.ERROR):
+        with pytest.raises(RuntimeError, match=expected_err):
+            get_provider({"marketplace_account": "cloud-emea", "auth": {"auth": "data"}})
+
+
+class TestCloudCredentials:
+    def test_invalid_credentials(self) -> None:
+        """Ensure the `cloud_name` must contain the prefix `-na` or `-emea`."""
+        expected_err = "Invalid value for cloud_name: missing region."
+        with pytest.raises(ValueError, match=expected_err):
+            CloudCredentials(cloud_name="foo")
+
+        assert CloudCredentials(cloud_name="foo-na")
+        assert CloudCredentials(cloud_name="foo-emea")
+
+
+class TestCloudProvider:
+    def test_abstract_class(self) -> None:
+        with pytest.raises(TypeError, match="Can't instantiate abstract class"):
+            CloudProvider()  # type: ignore
+
+    def test_raise_error_with_logs(self, caplog: LogCaptureFixture) -> None:
+        message = "something bad happended"
+        with caplog.at_level(logging.ERROR):
+            with pytest.raises(ValueError, match=message):
+                CloudProvider.raise_error(ValueError, message)
+        assert message in caplog.text
+
+    def test_default_post_actions(self, fake_provider: FakeProvider) -> None:
+        """Test the default behavior for `_post_upload` and `_post_publish`."""
+        push_item = create_autospec(VMIPushItem)
+
+        assert "Upload" == fake_provider._post_upload(push_item, "Upload")
+        assert "Publish" == fake_provider._post_publish(push_item, "Publish")
