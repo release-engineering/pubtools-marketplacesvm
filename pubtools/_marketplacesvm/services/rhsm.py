15fbea33a4bae7e24c2158b40477f6ae994f3ba6
Jonathan Gangi <jgangi@redhat.com>
2023-11-09 14:12:54 -0300

    Add the AWS RHSM Client SERVICE
    This commit implements the `Service` which will load the `AwsRHSMClient`
    for the main community push command.
    
    The `Service` is a mixin which will be added to the `CommunityVMPush` in
    order to bring the local `rhsm` client for it.
    
    Refers to EXDSP-2525

--- pubtools/_marketplacesvm/services/rhsm.py
+++ pubtools/_marketplacesvm/services/rhsm.py
@@ -262,3 +262,61 @@ class AwsRHSMClient(RHSMClient):
         LOG.debug("Listing all images from rhsm, %s", url)
         handle_page()
         return image_ids
+
+
+class AwsRHSMClientService(Service):
+    """A service providing RHSM client for AWS.
+
+    A client will be available only when RHSM url is provided.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the AwsRHSMClientService."""
+        self._lock = threading.Lock()
+        self._rhsm_instance = None
+        super(AwsRHSMClientService, self).__init__(*args, **kwargs)
+
+    def add_service_args(self, parser: ArgumentParser) -> None:
+        """Include the required arguments for the RHSM service.
+
+        Args:
+            parser (_type_): The parser to include the additional arguments.
+        """
+        super(AwsRHSMClientService, self).add_service_args(parser)
+
+        group = parser.add_argument_group("RHSM service")
+
+        group.add_argument("--rhsm-url", help="Base URL of the RHSM API")
+        group.add_argument(
+            "--rhsm-cert",
+            help="RHSM API certificate path (or set RHSM_CERT environment variable)",
+            type=from_environ("RHSM_CERT"),
+        )
+        group.add_argument(
+            "--rhsm-key",
+            help="RHSM API key path (or set RHSM_KEY environment variable)",
+            type=from_environ("RHSM_KEY"),
+        )
+
+    @property
+    def rhsm_client(self) -> 'AwsRHSMClient':
+        """RHSM client used for AMI related info on RHSM.
+
+        Error will be raised if the URL is not provided in the CLI.
+        """
+        with self._lock:
+            if not self._rhsm_instance:
+                self._rhsm_instance = self._get_rhsm_instance()
+        return self._rhsm_instance
+
+    def _get_rhsm_instance(self) -> 'AwsRHSMClient':
+        rhsm_url = self._service_args.rhsm_url
+        rhsm_cert = self._service_args.rhsm_cert
+        rhsm_key = self._service_args.rhsm_key
+        if not rhsm_url:
+            raise Exception("RHSM URL not provided for the RHSM client")
+
+        result = pm.hook.get_cert_key_paths(server_url=rhsm_url)  # pylint: disable=no-member
+        default_cert, default_key = result if result else (None, None)
+        cert = rhsm_cert or default_cert, rhsm_key or default_key
+        return AwsRHSMClient(rhsm_url, cert=cert)
