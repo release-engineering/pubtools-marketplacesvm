2813c2bc3e657eddfd9b89ea649d5dd7a593e785
Jonathan Gangi <jgangi@redhat.com>
2023-11-09 14:14:51 -0300

    Add the RHSM client and calls in community command
    This commit includes the `AwsRHSMClientService` into `CommunityVMPush`
    and implements the required methods to verify whether an `AmiPushItem`
    is present in RHSM or not.
    
    The goal is to only proceed with the upload whenever the product is
    already registered in RHSM.
    
    In a future MR it will be added the calls to update RHSM after the
    upload is done.
    
    Refers to EXDSP-2525

--- pubtools/_marketplacesvm/tasks/community_push/command.py
+++ pubtools/_marketplacesvm/tasks/community_push/command.py
@@ -3,23 +3,33 @@ import json
 import logging
 import os
 import sys
-from typing import Dict, Iterator, List, Union
+from typing import Any, Dict, Iterator, List, Optional, Union
 
-from attrs import asdict
+from attrs import asdict, evolve
 from more_executors import Executors
 from pushsource import AmiPushItem, Source
 from starmap_client.models import QueryResponse, Workflow
 
+from ...services.rhsm import AwsRHSMClientService
 from ..push import MarketplacesVMPush
 from ..push.items import MappedVMIPushItem, State
 
 log = logging.getLogger("pubtools.marketplacesvm")
 
 
-class CommunityVMPush(MarketplacesVMPush):
+class CommunityVMPush(MarketplacesVMPush, AwsRHSMClientService):
     """Upload an AMI to S3 and update RHSM."""
 
     _REQUEST_THREADS = int(os.environ.get("COMMUNITY_PUSH_REQUEST_THREADS", "5"))
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the CommunityVMPush instance."""
+        self._rhsm_products: Optional[List[Dict[str, Any]]] = None
+        super(CommunityVMPush, self).__init__(*args, **kwargs)
+
+    def _fail(self, *args, **kwargs):
+        log.error(*args, **kwargs)
+        sys.exit(30)
 
     @property
     def raw_items(self) -> Iterator[AmiPushItem]:
@@ -67,7 +77,8 @@ class CommunityVMPush(MarketplacesVMPush):
                 "starmap query returned for %s : %s ",
                 item.name,
                 json.dumps(
-                    {"name": binfo.name, "version": binfo.version, "query_response": asdict(query)}
+                    {"name": binfo.name, "version": binfo.version, "query_response": asdict(query)},
+                    default=str,
                 ),
             )
             item = MappedVMIPushItem(item, query.clouds)
@@ -77,6 +88,97 @@ class CommunityVMPush(MarketplacesVMPush):
             #    continue
             mapped_items.append({"item": item, "starmap_query": query})
         return mapped_items
+
+    @property
+    def rhsm_products(self) -> List[Dict[str, Any]]:
+        """List of products/image groups for AWS provider."""
+        if self._rhsm_products is None:
+            response = self.rhsm_client.aws_products().result()
+            self._rhsm_products = response.json()["body"]
+            prod_names = [
+                "%s(%s)" % (p["name"], p["providerShortName"]) for p in self._rhsm_products
+            ]
+            log.debug(
+                "%s Products(AWS provider) in rhsm: %s",
+                len(prod_names),
+                ", ".join(sorted(prod_names)),
+            )
+        return self._rhsm_products
+
+    def get_rhsm_product(self, product: str, image_type: str) -> Dict[str, Any]:
+        """Retrieve a product info from RHSM for the specified product in metadata.
+
+        Args:
+            product (str): The product name
+            image_type (str): The image type (hourly or access)
+
+        Returns:
+            The specified product info from RHSM.
+        """
+        # The rhsm prodcut should always be the product (short) plus
+        # "_HOURLY" for hourly type images.
+        image_type = image_type.upper()
+        aws_provider_name = self.args.aws_provider_name
+        if image_type == "HOURLY":
+            product = product.upper() + "_" + image_type
+
+        log.debug(
+            "Searching for product %s for provider %s in rhsm",
+            product,
+            aws_provider_name,
+        )
+        for rhsm_product in self.rhsm_products:
+            if (
+                rhsm_product["name"] == product
+                and rhsm_product["providerShortName"] == aws_provider_name  # noqa: W503
+            ):
+                return rhsm_product
+
+        raise RuntimeError("Product not in RHSM: %s" % product)
+
+    def in_rhsm(self, product: str, image_type: str) -> bool:
+        """Check whether the product is present in rhsm for the provider.
+
+        Args:
+            product (str): The product name
+            image_type (str): The image type (hourly or access)
+            aws_provider_name (str): The AWS provider name
+
+        Returns:
+            True if the product is found in rhsm_products else False.
+        """
+        try:
+            self.get_rhsm_product(product, image_type)
+        except RuntimeError as er:
+            log.error(er)
+            return False
+        return True
+
+    def items_in_metadata_service(self, mapped_items: List[MappedVMIPushItem]):
+        """Check for all the push_items whether they are in rhsm or not.
+
+        Args:
+            mapped_items:
+                List of MappedVMIPushItem to check on RHSM.
+
+        Returns:
+            False if any of item is missing in RHSM else True.
+        """
+        verified = True
+        for item in mapped_items:
+            # Since the StArMap "meta" should be the same for all destinations
+            # we can retrieve a push item from any marketplace at this moment
+            # just to have all properties loaded before checking on RHSM
+            pi = item.get_push_item_for_marketplace(item.marketplaces[0])
+            if not self.in_rhsm(pi.release.product, pi.type):
+                log.error(
+                    "Pre-push check in metadata service failed for %s at %s",
+                    pi.name,
+                    pi.src,
+                )
+                item.push_item = evolve(pi, state="INVALIDFILE")
+                verified = False
+        return verified
 
     def _push_to_community(
         self, mapped_item: MappedVMIPushItem, starmap_query: QueryResponse
@@ -95,9 +197,22 @@ class CommunityVMPush(MarketplacesVMPush):
         log.debug("PushItem: %s", mapped_item)
         raise NotImplementedError("Not implemented yet")
 
+    def add_args(self):
+        """Include the required CLI arguments for CommunityVMPush."""
+        super(CommunityVMPush, self).add_args()
+
+        self.parser.add_argument(
+            "--aws-provider-name",
+            help="AWS provider e.g. AWS, ACN (AWS China), AGOV (AWS US Gov)",
+            default="AWS",
+        )
+
     def run(self):
         """Execute the community_push command workflow."""
         mapped_items = self.mapped_items
+        if not self.items_in_metadata_service([x["item"] for x in mapped_items]):
+            self._fail("Pre-push verification of push items in metadata service failed")
+
         executor = Executors.thread_pool(
             name="pubtools-marketplacesvm-community-push",
             max_workers=min(max(len(mapped_items), 1), self._REQUEST_THREADS),
@@ -125,8 +240,7 @@ class CommunityVMPush(MarketplacesVMPush):
         self.collect_push_result(result)
 
         if failed:
-            log.error("Community VM push failed")
-            sys.exit(30)
+            self._fail("Community VM push failed")
 
         # FIXME: Remove the coverage skip when the command gets properly implemented
         log.info("Community VM push completed")  # pragma: no cover
