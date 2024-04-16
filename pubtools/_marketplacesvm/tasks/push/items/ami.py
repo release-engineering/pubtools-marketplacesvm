15764c9527a65b29075459cfb45caa036227708d
Jonathan Gangi <jgangi@redhat.com>
2024-04-16 09:49:35 -0300

    Add AWS AccessEndpointUrl conversion
    This commit includes the AccessEndpointUrl conversion for AmiPushItems
    in order to properly allow the StArMap attributes mappings.
    
    Refers to SPSTRAT-205

--- pubtools/_marketplacesvm/tasks/push/items/ami.py
+++ pubtools/_marketplacesvm/tasks/push/items/ami.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any, Dict, List
 
-from pushsource import AmiSecurityGroup
+from pushsource import AmiAccessEndpointUrl, AmiSecurityGroup
 
 from .starmap import MappedVMIPushItem
 
@@ -22,4 +22,19 @@ def aws_security_groups_converter(value: List[Dict[str, Any]]) -> List[AmiSecuri
     return [AmiSecurityGroup._from_data(x) for x in value]
 
 
+def aws_access_endpoint_url_converter(value: Dict[str, Any]) -> AmiAccessEndpointUrl:
+    """
+    Convert data to AmiAccessEndpointUrl.
+
+    Args:
+        value (Dict[str, Any]): Access endpoint URL to convert.
+
+    Returns:
+        AmiAccessEndpointUrl: The converted AmiAccessEndpointUrl.
+    """
+    log.debug("Converting data to AmiAccessEndpointUrl: %s", value)
+    return AmiAccessEndpointUrl._from_data(value)
+
+
 MappedVMIPushItem.register_converter("security_groups", aws_security_groups_converter)
+MappedVMIPushItem.register_converter("access_endpoint_url", aws_access_endpoint_url_converter)
