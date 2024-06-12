5b6815e80a3c1871aa49f8f14835db003aa4eda5
Jonathan Gangi <jgangi@redhat.com>
2024-06-12 16:02:16 -0300

    Add push from advisory test
    Add a test in which there are 2 builds attached to an advisory:
    
    - 1st build is pushed in both workflows
    - 2nd build doesn't have any mappings in StArMap
    
    The outcome is that the push fails.
    
    Refers to SPSTRAT-269

--- tests/combined_push/test_combined_push.py
+++ tests/combined_push/test_combined_push.py
@@ -1,11 +1,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import re
 import time
-from typing import Type, Union
+from copy import deepcopy
+from typing import Any, Dict, Type, Union
 from unittest import mock
 
 import pytest
-from pushsource import AmiPushItem
+from attrs import evolve
+from pushsource import AmiPushItem, AmiRelease, KojiBuildInfo
 from starmap_client.models import QueryResponse
 
 from pubtools._marketplacesvm.cloud_providers import CloudProvider
@@ -259,3 +261,61 @@ def test_do_marketplace_push(
         ],
     )
     community_source.get.assert_not_called()
+
+
+@mock.patch("pubtools._marketplacesvm.tasks.community_push.command.Source")
+@mock.patch("pubtools._marketplacesvm.tasks.push.command.Source")
+def test_do_advisory_push(
+    marketplace_source: mock.MagicMock,
+    community_source: mock.MagicMock,
+    ami_push_item: AmiPushItem,
+    release_params: Dict[str, Any],
+    starmap_query_aws_marketplace: QueryResponse,
+    starmap_query_aws_community: QueryResponse,
+    monkeypatch: pytest.MonkeyPatch,
+    command_tester: CommandTester,
+) -> None:
+    """Test two builds which one contains mappings for both workflows and the other doesn't."""
+    # Set starmap to only provide mappings for the first push item
+    mock_starmap = mock.MagicMock()
+    mock_starmap.query_image_by_name.side_effect = [
+        starmap_query_aws_marketplace,
+        None,
+        starmap_query_aws_community,
+        None,
+    ]
+    monkeypatch.setattr(CommunityVMPush, 'starmap', mock_starmap)
+    monkeypatch.setattr(MarketplacesVMPush, 'starmap', mock_starmap)
+
+    # Setup a second push item which will not have mappings
+    second_release = deepcopy(release_params)
+    second_release["product"] = "second_product"  # different name with no mappings
+    second_release["date"] = "2024-01-01"
+    second_binfo = KojiBuildInfo(name="second-build", version="8.0", release="11111111")
+    second_build = deepcopy(ami_push_item)
+    second_build = evolve(
+        second_build,
+        name="second_push_item",
+        build_info=second_binfo,
+        release=AmiRelease(**second_release),
+    )
+
+    # Add push items in the queue
+    marketplace_source.get.return_value.__enter__.return_value = [ami_push_item, second_build]
+    community_source.get.return_value.__enter__.return_value = [ami_push_item, second_build]
+
+    # Test
+    command_tester.test(
+        lambda: entry_point(CombinedVMPush),
+        [
+            "test-push",
+            "--starmap-url",
+            "https://starmap-example.com",
+            "--credentials",
+            "eyJtYXJrZXRwbGFjZV9hY2NvdW50IjogInRlc3QtbmEiLCAiYXV0aCI6eyJmb28iOiJiYXIifQo=",
+            "--rhsm-url",
+            "https://rhsm.com/test/api/",
+            "--debug",
+            "koji:https://fakekoji.com?vmi_build=ami_build,ami_build2",
+        ],
+    )
