b9409a9297d6c985521a0b26d69e49eb6fa35480
Jonathan Gangi <jgangi@redhat.com>
2025-07-30 14:47:18 -0300

    SPSTRAT-575: Iterate over all marketplaces before deleting items
    This commit fixes a bug which was preventing the deletion of some images
    on 2 or more marketplaces which didn't have their marketplaces declared
    in the field `cloud_info`.
    
    Refers to SPSTRAT-575
    
    Signed-off-by: Jonathan Gangi <jgangi@redhat.com>

--- tests/delete/test_delete.py
+++ tests/delete/test_delete.py
@@ -162,8 +162,7 @@ def test_delete_vhd(
 
     fake_azure_source.get.assert_called_once()
     assert fake_cloud_instance.call_count == 1
-    for call in fake_cloud_instance.call_args_list:
-        assert call.args == ('azure-na',)
+    assert fake_cloud_instance.call_args_list[0].args == ('azure-na',)
 
 
 @mock.patch("pubtools._marketplacesvm.tasks.delete.command.Source")
